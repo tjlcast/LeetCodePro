893f31158610ac95e5c99f16f3185925a27bcbca
tangjialiang <phx108@sina.com>
2017-05-01 21:45:39 +0800

    add!

---
+++ src/HashMap/_49_Group_Anagrams.java
@@ -0,0 +1,39 @@
+package HashMap;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class _49_Group_Anagrams {
+
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public List<List<String>> work (String[] strs) {
+		return new Solution().groupAnagrams(strs) ;
+	}
+	
+	public class Solution {
+	    public List<List<String>> groupAnagrams(String[] strs) {
+	    	HashMap<String, List<String>> dict = new HashMap() ;
+	        for(String s : strs) {
+	        	char[] charL = s.toCharArray() ;
+	        	Arrays.sort(charL);
+	        	System.out.println(charL);
+	        	String key = new String(charL) ;
+	        	List<String> value = dict.get(key)!=null ? dict.get(key) : new LinkedList<String>() ;
+	        	value.add(s) ;
+	        	dict.put(key, value) ;
+	        }
+	        LinkedList<List<String>> res = new LinkedList<>() ;
+	        for(List<String> v : dict.values()){
+	        	res.add(v) ;
+	        }
+	        return res ;
+	    }
+	}
+
+}
