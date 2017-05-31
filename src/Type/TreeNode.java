7817575de1f1079a899442b7a8d4842a07ee5bb3
tangjialiang <phx108@sina.com>
2017-05-31 21:26:00 +0800

    add

---
+++ src/Type/TreeNode.java
@@ -0,0 +1,13 @@
+package Type;
+
+/**
+ * Created by tangjialiang on 2017/5/31.
+ */
+
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
