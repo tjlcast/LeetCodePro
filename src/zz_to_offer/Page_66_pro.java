d8f4f031d4249853ef5baddb8740d93f9c87b1ec
tangjialiang <phx108@sina.com>
2017-08-30 20:38:13 +0800

    add page age 66

---
+++ src/zz_to_offer/Page_66_pro.java
@@ -0,0 +1,75 @@
+package zz_to_offer;
+
+/**
+ * Created by tangjialiang on 2017/8/22.
+ */
+public class Page_66_pro {
+    /**
+     * 在旋转数组里找到指定的数字。
+     */
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 8, 1, 2, 3} ;
+        int k = 1 ;
+        int work = new Page_66_pro().work(nums, k, 0, nums.length - 1);
+        System.out.println("work is: " + work) ;
+    }
+
+    public int work(int[] nums, int k, int begin, int end) {
+        return new MySolution().findNumFromInput(nums, k, begin, end) ;
+    }
+
+    class MySolution {
+        /**
+         * if containing return the postition, otherwise return -1
+         * @param nums
+         * @param k
+         * @param begin
+         * @param end
+         * @return the position or flag
+         */
+        public int findNumFromInput(int[] nums, int k, int begin, int end) {
+            int mid = (begin + end) / 2 ;
+            int midNum = nums[mid] ;
+
+            if (midNum == k) return mid ;
+            if (begin == end) return -1 ;
+
+            if (midNum >= nums[begin]) {
+                if (k > midNum || k < nums[begin]) {
+                    return findNumFromInput(nums, k, mid+1, end) ;
+                } else {
+                    // use bSearch find [begin, mid)
+                    // todo
+                    return bSearch(nums, k, begin, mid-1) ;
+                }
+            } else {
+                if (k < midNum || k > nums[end]) {
+                    return findNumFromInput(nums, k, begin, mid-1) ;
+                } else {
+                    // use bSearch find (mid, end]
+                    // todo
+                    return bSearch(nums, k, mid+1, end) ;
+                }
+            }
+        }
+
+        private int bSearch(int[] nums, int k, int begin, int end) {
+
+            while(begin != end) {
+                int mid = (begin + end) / 2 ;
+                if (nums[mid] == k) return mid ;
+
+                if (k > nums[mid]) {
+                    begin = mid + 1 ;
+                }
+                if (k < nums[mid]) {
+                    end = mid - 1 ;
+                }
+            }
+
+            if (nums[begin] == k) return begin ;
+
+            return -1 ;
+        }
+    }
+}
