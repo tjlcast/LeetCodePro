680e7455249236785105f7ca90379a46b9891d42
tangjialiang <phx108@sina.com>
2017-11-27 09:48:45 +0800

    add

--- src/LeetCode_Offer_Offer_Offer/_126_Word_Ladder_II.java
+++ src/LeetCode_Offer_Offer_Offer/_126_Word_Ladder_II.java
@@ -10,28 +10,28 @@ import java.util.List;
  */
 public class _126_Word_Ladder_II {
     /**
-     * 既然是求最短路径，可以考虑动归或广搜。这道题对字典直接进行动归是不现实的，因为字典里的单词非常多。只能选择广搜了。
+     * Given two words (beginWord and endWord), and a dictionary's word list, find all shortest transformation sequence(s) from beginWord to endWord, such that:
 
-     思路也非常直观，从start或end开始，不断加入所有可到达的单词，直到最终到达另一端。本质上广度优先遍历图。
+     Only one letter can be changed at a time
+     Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
+     For example,
 
-     需要注意的是，拓展下一个单词时不能对字典进行枚举，因为字典里的单词太多。幸好单词本身都不长，所以直接枚举单词所有可能的变形，看看在dict中出现没有。
-
-     当然还不止这些，上面的做法仍然会超时，需要添加剪枝策略：
-
-     1. 如果某个单词在以前遍历过了，那么以后都不用再考虑，因为之后遍历到的路径一定不是最短的
-
-     2. 在广搜法拓展下一轮单词时，注意去重
-
-     此外还需要注意的是，不能把每个单词到start或end的路径都保存下来，那样内存会爆掉。所以要压缩保存结果，通常的做法是用一个map保存当前单词下一步是什么单词。例如next[word] = {next_word1, next_word2, next_word3...}。最后从next[start]开始再次使用广度优先搜索法构造出所有解。
-
-     算法不难，但是编码非常容易出错，所以总体上还是挺难的。最后运行时间640ms，还是有挺大优化空间的。
+     Given:
+     beginWord = "hit"
+     endWord = "cog"
+     wordList = ["hot","dot","dog","lot","log","cog"]
+     Return
+     [
+     ["hit","hot","dot","dog","cog"],
+     ["hit","hot","lot","log","cog"]
+     ]
      * @param args
      */
 
     public static void main(String[] args) {
-        String beginWord = "red" ;
-        String endWord = "tax" ;
-        String[] arr = {"ted","tex","red","tax","tad","den","rex","pee"} ;
+        String beginWord = "hit" ;
+        String endWord = "cog" ;
+        String[] arr = {"hot","dot","dog","lot","log"} ;
         LinkedList<String> l = new LinkedList<>() ;
         for(String s : arr) {
             l.addLast(s);
@@ -54,6 +54,17 @@ public class _126_Word_Ladder_II {
     }
 
     public class Solution {
+        /**
+         * 既然是求最短路径，可以考虑动归或广搜。这道题对字典直接进行动归是不现实的，因为字典里的单词非常多。只能选择广搜了。
+         思路也非常直观，从start或end开始，不断加入所有可到达的单词，直到最终到达另一端。本质上广度优先遍历图。
+         需要注意的是，拓展下一个单词时不能对字典进行枚举，因为字典里的单词太多。幸好单词本身都不长，所以直接枚举单词所有可能的变形，看看在dict中出现没有。
+         当然还不止这些，上面的做法仍然会超时，需要添加剪枝策略：
+         1. 如果某个单词在以前遍历过了，那么以后都不用再考虑，因为之后遍历到的路径一定不是最短的
+         2. 在广搜法拓展下一轮单词时，注意去重
+         此外还需要注意的是，不能把每个单词到start或end的路径都保存下来，那样内存会爆掉。所以要压缩保存结果，通常的做法是用一个map保存当前单词下一步是什么单词。例如next[word] = {next_word1, next_word2, next_word3...}。最后从next[start]开始再次使用广度优先搜索法构造出所有解。
+         算法不难，但是编码非常容易出错，所以总体上还是挺难的。最后运行时间640ms，还是有挺大优化空间的。
+         */
+
         List<List<String>> ans = new LinkedList<>() ;
         HashMap<String, List<String>> parents = new HashMap<>() ;
         HashMap<String, Boolean> isVisited = new HashMap<>() ;
@@ -65,55 +76,38 @@ public class _126_Word_Ladder_II {
             this.beginWord = beginWord ;
             this.endWord = endWord ;
 
-            String roundEnd = "" ;
-            LinkedList<String> queue = new LinkedList<>() ;
-            queue.addLast(beginWord);
-            queue.addLast(roundEnd) ;
-            isVisited.put(beginWord, true) ;
-
-            boolean flag = false ; // for bfs --> is find the endWord.
-
-            while (queue.size()!=1) {
-                String curStr = queue.removeFirst();
-                if (curStr == roundEnd) {
-                    if (flag) { System.out.println("end"); break ; }
-                    queue.addLast(roundEnd);
-                    continue;
-                }
-
-                isVisited.put(curStr, true) ;
-
-                for(String candidate : wordList) {
-                    if(goByStep(candidate, curStr) && isVisited.getOrDefault(candidate, false)==false) {
-                        if (candidate.equals(endWord)) {
-                            flag = true ;
-                        }
-
-                        List<String> ps = parents.getOrDefault(candidate, new LinkedList<>());
-                        if(!isVisited.getOrDefault(candidate, false))ps.add(curStr) ;
-                        parents.put(candidate, ps) ;
-
-                        if(!queue.contains(candidate) && !flag)queue.addLast(candidate);
-                    }
-                }
-            }
-
             // use bfs to generate the parents
+            LinkedList<String> queue = new LinkedList<>() ;
             queue = new LinkedList<>() ;
             queue.addLast(beginWord);
+            boolean isExpand = true ;
 
-            while (true) {
-                // from old queue to
+            while (isExpand) {
+                // from old queue to new queue    (tax ->  tex tad tex)
+                LinkedList<String> newQueue = new LinkedList<>() ;
+                if(queue.isEmpty()) break ;
+
+                while(!queue.isEmpty()){
+                    String s = queue.removeFirst();
+
+                    isVisited.put(s, true) ;
+
+                    for(String candidate : wordList) {
+                        if (goByStep(candidate, s) && isVisited.getOrDefault(candidate, false)==false && !queue.contains(candidate)) {
+                            if (candidate.equals(endWord)) {
+                                isExpand = false;
+                            }
+
+                            List<String> ps = parents.getOrDefault(candidate, new LinkedList<>());
+                            ps.add(s);
+                            parents.put(candidate, ps);
+
+                            if (!queue.contains(candidate) && !newQueue.contains(candidate)) newQueue.addLast(candidate);
+                        }
+                    }
+                }
+                queue = newQueue ;
             }
-
-            // test
-//            for(String key : parents.keySet()) {
-//                System.out.print(key + " -> ") ;
-//                for(String val : parents.get(key)) {
-//                    System.out.print(" " + val) ;
-//                }
-//                System.out.println() ;
-//            }
 
             // get parents
             try {
