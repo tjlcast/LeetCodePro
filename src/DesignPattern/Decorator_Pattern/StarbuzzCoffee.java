cf7718fbc83dcb2410293df0e87e7202abd778e2
tangjialiang <phx108@sina.com>
2018-01-25 17:04:23 +0800

    add decorator pattern

---
+++ src/DesignPattern/Decorator_Pattern/StarbuzzCoffee.java
@@ -0,0 +1,24 @@
+package DesignPattern.Decorator_Pattern;
+
+/**
+ * Created by tangjialiang on 2018/1/25.
+ */
+public class StarbuzzCoffee {
+    /**
+     * 拿一个Espresso咖啡
+     * 以Mocha对象来装饰它
+     * 调用cost()方法，并依赖委托将调料的价格加上去
+     *
+     * 装饰模式对某个接口的实现进行再包装。
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Beverage beverage = new Espresso() ;
+        System.out.println(beverage.getDescription()
+        + " $" + beverage.cost()) ;
+
+        Beverage beverage1 = new Espresso() ;
+        beverage1 = new Mocha(beverage1) ;
+    }
+}
