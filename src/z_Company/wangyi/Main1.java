7b0ce3c940142e50c4956aabad14751c9b7ecf37
tangjialiang <phx108@sina.com>
2018-03-28 12:23:31 +0800

    adjust the code..

--- src/z_Company/wangyi/Main1.java
+++ src/z_Company/wangyi/Main1.java
@@ -1,24 +1,73 @@
 package z_Company.wangyi;
 
-/**
- * Created by tangjialiang on 2018/3/27.
- */
-
 import java.util.* ;
 
+/**
+ * Created by tangjialiang on 2018/3/28.
+ */
+
+/**
+ * 2
+ * 0 90
+ * 0 90
+ * 100 200
+ * 100 200
+ */
+
 public class Main1 {
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n ;
-        int k ;
+        Scanner sc = new Scanner(System.in) ;
 
-        n = in.nextInt();
-        k = in.nextInt();
-
+        int n = sc.nextInt() ;
+        int[] x1 = new int[n] ;
+        int[] y1 = new int[n] ;
+        int[] x2 = new int[n] ;
+        int[] y2 = new int[n] ;
+        HashSet<Integer> allX = new HashSet<>() ;
         int ans = 0 ;
-        for(int x=n; x>=1; x--) {
-            for(int y=n; y>=1; y--) {
-                if (x % y >= k) ans++ ;
+
+        for (int i = 0; i < n; i++) {
+            x1[i] = sc.nextInt() ;
+            allX.add(x1[i]) ;
+        }
+        for (int i = 0; i < n; i++) {
+            y1[i] = sc.nextInt() ;
+        }
+        for (int i = 0; i < n; i++) {
+            x2[i] = sc.nextInt() ;
+            allX.add(x2[i]) ;
+        }
+        for (int i = 0; i < n; i++) {
+            y2[i] = sc.nextInt() ;
+        }
+
+        for(Integer x_a : allX) {
+            for(Integer x_b : allX) {
+                if (x_a == x_b) continue;
+                HashMap<Integer, Integer> inPoints = new HashMap<>() ;
+                HashMap<Integer, Integer> outPoints = new HashMap<>() ;
+                LinkedList<Integer> points = new LinkedList<>();
+                for (int i = 0; i < n; i++) {
+                    if (Math.min(x_a,x_b)<=x1[i] && x2[i]<=Math.max(x_a, x_b)) {
+                        inPoints.put(Math.max(y1[i], y2[i]), inPoints.getOrDefault(Math.max(y1[i], y2[i]), 0)+1) ;
+                        outPoints.put(Math.min(y1[i], y2[i]), outPoints.getOrDefault(Math.min(y1[i], y2[i]), 0)+1) ;
+                        points.add(y1[i]) ;
+                        points.add(y2[i]) ;
+                    }
+                }
+                Collections.sort(points, new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o2-o1;
+                    }
+                });
+                int curH = 0 ;
+                for(Integer point : points) {
+                    curH += inPoints.getOrDefault(point, 0) ;
+                    curH -= outPoints.getOrDefault(point, 0) ;
+                    ans = Math.max(ans, curH) ;
+                }
             }
         }
 
--- src/z_Company/wangyi/Main3.java
+++ src/z_Company/wangyi/Main3.java
@@ -6,16 +6,83 @@ package z_Company.wangyi;
 import java.util.* ;
 
 /**
- 3 3
- 10 1000
- 1 100
- 1000000000 1001
- 9 10 1000000000
+3 3
+10 1000
+1 100
+1000000000 1001
+9 10 1000000000
  */
 
 public class Main3 {
 
     public static void main(String[] args) {
+        solution2() ;
+    }
+
+    /**
+     * by tjlcast
+     *
+     * sort cost and max profit
+     * binearySearch on cost to get max profit.
+     */
+    public static void solution2() {
+        Scanner sc = new Scanner(System.in) ;
+        int works = sc.nextInt() ;
+        int friends = sc.nextInt() ;
+        int[] abilities = new int[friends] ;
+        int[][] arr = new int[works][2] ;
+
+        for(int i=0; i<works; i++) {
+            int cost = sc.nextInt() ;
+            int val = sc.nextInt() ;
+
+            arr[i][0] = cost ;
+            arr[i][1] = val ;
+        }
+
+        for(int i=0; i<works; i++) {
+            abilities[i] = sc.nextInt() ;
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] - o2[0];
+            }
+        });
+
+        int localMax = 0 ;
+        for(int i=0; i<works; i++) {
+            arr[i][1] = (localMax = Math.max(localMax, arr[i][1])) ;
+        }
+
+        for(int i=0; i<friends; i++) {
+            int ability = abilities[i] ;
+
+            int l=0, r=works-1;
+            while(l<r) {
+                int mid = ((r-l)>>1) + l + 1 ;
+                if (arr[mid][0] == ability) {
+                    break ;
+                } else if (arr[mid][0] > ability) {
+                    r = mid-1 ;
+                } else {
+                    l = mid ;
+                }
+            }
+            System.out.println(arr[r][1]) ;
+        }
+    }
+
+    /**
+     * by cc
+     *
+     * sort cost and max profit
+     * binearySearch on cost to get max profit.
+     *
+     * (better use TreeMap, the key of ts is sorted and could use bs on it's key. )
+     */
+    public static void solution1() {
         Scanner sc = new Scanner(System.in);
         int works = sc.nextInt();
         int friends = sc.nextInt();
