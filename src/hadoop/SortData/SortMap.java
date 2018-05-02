5810d5ea11e77ca94366c1a9c8e11ad67356168f
tangjialiang <phx108@sina.com>
2018-05-02 11:33:50 +0800

    some easy action about hadoop !!!!

--- src/hadoop/SortData/SortMap.java
+++ src/hadoop/SortData/SortMap.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 
 /**
  * Created by tangjialiang on 2018/5/2.
+ *
+ * 该app的reducer应该只能运行一个实例，使用到了局部数据（成员变脸）
  */
 public class SortMap extends Mapper<LongWritable, Text, IntWritable, Text> {
     @Override
