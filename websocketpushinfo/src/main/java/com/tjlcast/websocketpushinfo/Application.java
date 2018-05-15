0bc0c17fdb3d7785b440d8ae1fcb0e6b534d16ab
tangjialiang <phx108@sina.com>
2018-05-15 22:40:12 +0800

    change to be modules

---
+++ websocketpushinfo/src/main/java/com/tjlcast/websocketpushinfo/Application.java
@@ -0,0 +1,15 @@
+package com.tjlcast.websocketpushinfo;
+
+import org.springframework.boot.SpringApplication ;
+import org.springframework.boot.autoconfigure.SpringBootApplication ;
+
+/**
+ * Created by tangjialiang on 2018/5/15.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run() ;
+    }
+}
